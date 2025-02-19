09f99ae417a7730672b39a289d46c7d2f34cab40
nunes <vera.ml.nunes@gmail.com>
2025-02-19 17:19:34 +0000

    Summary

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -4,6 +4,10 @@ namespace MeuPrograma
 {
     public class Program
     {
+        /// <summary>
+        /// write in terminal the given lines
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
